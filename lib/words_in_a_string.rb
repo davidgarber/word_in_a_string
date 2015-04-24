f5b2fe7b542551fb3a65d28ab20bd1a9421eec91
David Garber <davidgarber@gmail.com>
2015-04-24 14:06:14 -0700

    Attempt to imitate anagrams lib.rb which was how I began. fails test

--- lib/words_in_a_string.rb
+++ lib/words_in_a_string.rb
@@ -1,10 +1,13 @@
 class String
-  define_method(:words_in_a_string) do
-    new_array = []
-    total = 0
-    words = self.split() do |chosen_word|
-    count(chosen_word)
-    total = total.+(chosen_word)
+  define_method(:words_in_a_string) do |chosen_word|
+    phrase = self
+    phrase_words = phrase.split('')
+    output = []
+    phrase_words.each do |word|
+      if chosen_word = word
+        count()
+    end
   end
+   return count()
  end
-end
+ end
