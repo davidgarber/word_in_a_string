a5be18b34502d9d4e3e5d16eca4871054c5e0e43
David Garber <davidgarber@gmail.com>
2015-04-24 13:02:16 -0700

    tried it as a hash for hours until I realized a hash needs a key and a value. fails test.

--- lib/words_in_a_string.rb
+++ lib/words_in_a_string.rb
@@ -1,11 +1,14 @@
 class String
   define_method(:words_in_a_string) do |chosen_word|
-    word_array = self.split()
+    word_hash = self.split{}
     word_count = 0
-    word_array.each do |word|
-      if chosen_word = word
-        word_count += 1
-      end
+    word_hash.each do |chosen_word|
+      temp_count = word_hash.fetch(chosen_word)
+      word_count = word_count.+(temp_count)
+
+      # if chosen_word = word
+      #   word_count += 1
+      #end
     end
     word_count
   end
--- spec/words_in_a_string_spec.rb
+++ spec/words_in_a_string_spec.rb
@@ -5,4 +5,8 @@ describe('String#words_in_a_string') do
   it('it returns 1 when the number of times the word is in a string once') do
     expect("the".words_in_a_string("the car is red")).to(eq(1))
   end
+
+  it('it returns 2 when the number of times the word is in the string twice') do
+    expect("car".words_in_a_string("the car is a car")).to(eq(2))
+  end
 end
