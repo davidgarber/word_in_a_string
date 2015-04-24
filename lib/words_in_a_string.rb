9d57ef64e7d2a6515156294795bdfdd014abbff6
David Garber <davidgarber@gmail.com>
2015-04-24 13:20:44 -0700

    Change specs from 'the' ('the car is red') to 'the car is red' to ('the'), switch arguments. fails test.

--- lib/words_in_a_string.rb
+++ lib/words_in_a_string.rb
@@ -1,15 +1,6 @@
 class String
   define_method(:words_in_a_string) do |chosen_word|
-    word_hash = self.split{}
-    word_count = 0
-    word_hash.each do |chosen_word|
-      temp_count = word_hash.fetch(chosen_word)
-      word_count = word_count.+(temp_count)
-
-      # if chosen_word = word
-      #   word_count += 1
-      #end
-    end
-    word_count
+    self.split().count(chosen_word)
+      return (chosen_word)
   end
 end
--- spec/words_in_a_string_spec.rb
+++ spec/words_in_a_string_spec.rb
@@ -3,10 +3,10 @@ require('./lib/words_in_a_string.rb')
 
 describe('String#words_in_a_string') do
   it('it returns 1 when the number of times the word is in a string once') do
-    expect("the".words_in_a_string("the car is red")).to(eq(1))
+    expect("the car is red".words_in_a_string("the")).to(eq(1))
   end
 
   it('it returns 2 when the number of times the word is in the string twice') do
-    expect("car".words_in_a_string("the car is a car")).to(eq(2))
+    expect("the car is a car".words_in_a_string("car")).to(eq(2))
   end
 end
