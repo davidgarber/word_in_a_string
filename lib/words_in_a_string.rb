9f99bda7d64c3ea7f518255e84aad6dcf34c0f1f
David Garber <davidgarber@gmail.com>
2015-04-24 14:33:00 -0700

    change to find and replace method if chosen_word is there, create array, split and count

--- lib/words_in_a_string.rb
+++ lib/words_in_a_string.rb
@@ -1,13 +1,9 @@
 class String
   define_method(:words_in_a_string) do |chosen_word|
-    phrase = self
-    phrase_words = phrase.split('')
-    output = []
-    phrase_words.each do |word|
-      if chosen_word = word
-        count()
+    if self.include?(chosen_word)
+      new_array = self.split().count(chosen_word)
+
+
     end
-  end
-   return count()
- end
- end
+end
+end
