b7c7a269bf4aaf76b3e0af058d85d1656c72f4d8
David Garber <davidgarber@gmail.com>
2015-04-24 13:46:10 -0700

    Tries to split string into array, set chosen word as parameter, count chosen word. fails test.

--- lib/words_in_a_string.rb
+++ lib/words_in_a_string.rb
@@ -1,6 +1,10 @@
 class String
-  define_method(:words_in_a_string) do |chosen_word|
-    self.split().count(chosen_word)
-      return (chosen_word)
+  define_method(:words_in_a_string) do
+    new_array = []
+    total = 0
+    words = self.split() do |chosen_word|
+    count(chosen_word)
+    total = total.+(chosen_word)
   end
+ end
 end
