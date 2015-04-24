aa9f8658fbee056fc113e8717b74916fda61c3ba
David Garber <davidgarber@gmail.com>
2015-04-24 14:35:49 -0700

    third spec to return zero when no words are found in string. fails test.

--- spec/words_in_a_string_spec.rb
+++ spec/words_in_a_string_spec.rb
@@ -9,4 +9,9 @@ describe('String#words_in_a_string') do
   it('it returns 2 when the number of times the word is in the string twice') do
     expect("the car is a car".words_in_a_string("car")).to(eq(2))
   end
+
+  it("it returns 0 when the number of times the word is in the string is none") do
+    expect("the car is red".words_in_a_string("boy")).to(eq(0))
+  end
+
 end
