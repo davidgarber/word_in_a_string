cb4656a0e2a171c67266e67988a9790acecf071d
David Garber <davidgarber@gmail.com>
2015-04-24 15:04:46 -0700

    create words_in_a_string_integration_spec uninitialized constant Sinatra fails test

--- lib/words_in_a_string.rb
+++ lib/words_in_a_string.rb
@@ -2,8 +2,8 @@ class String
   define_method(:words_in_a_string) do |chosen_word|
     if self.include?(chosen_word)
       new_array = self.split().count(chosen_word)
-
-
+    else
+      #struggled to find else statement will try and complete and return
     end
-end
+  end
 end
---
+++ spec/words_in_a_string_integrated_spec.rb
@@ -0,0 +1,13 @@
+require('capybara/rspec')
+require('./app')
+Capybara.app = Sinatra::Application
+
+describe('finding the number of words in a string', {:type => :feature}) do
+  it('takes a word as an input and a string as an input and tells how many times that word appears') do
+    visit('/')
+    fill_in('word', with => "the")
+    fill_in('string', with => "the car is red")
+    click_button('Send')
+    expect(page).to have_content(1)
+  end
+end
